9b6764d53ea81a76c25d41183189d482aebe0f8e
Daniel Li <daniel88_li@hotmail.com>
2020-04-07 21:12:47 -0400

    Renamed the class and removed unused imports

--- app/src/androidTest/java/org/mozilla/focus/activity/OpenNewTabTest.java
+++ app/src/androidTest/java/org/mozilla/focus/activity/OpenNewTabTest.java
@@ -9,64 +9,36 @@ import android.preference.PreferenceManager;
 import android.widget.RadioButton;
 
 import androidx.test.InstrumentationRegistry;
-import androidx.test.espresso.IdlingRegistry;
-import androidx.test.espresso.web.webdriver.Locator;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
-import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiScrollable;
 import androidx.test.uiautomator.UiSelector;
-import androidx.test.uiautomator.Until;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mozilla.focus.R;
-import org.mozilla.focus.ext.ContextKt;
-import org.mozilla.focus.helpers.SessionLoadedIdlingResource;
+import org.mozilla.focus.fragment.FirstrunFragment;
 import org.mozilla.focus.helpers.TestHelper;
-import org.mozilla.focus.utils.AppConstants;
-import org.mozilla.focus.web.IWebView;
-
-import mozilla.components.browser.session.SessionManager;
-import okhttp3.mockwebserver.MockWebServer;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.hasFocus;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.espresso.web.assertion.WebViewAssertions.webMatches;
-import static androidx.test.espresso.web.sugar.Web.onWebView;
-import static androidx.test.espresso.web.webdriver.DriverAtoms.findElement;
-import static androidx.test.espresso.web.webdriver.DriverAtoms.getText;
 import static junit.framework.TestCase.assertTrue;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-import static org.mozilla.focus.fragment.FirstrunFragment.FIRSTRUN_PREF;
-import static org.mozilla.focus.helpers.EspressoHelper.*;
-import static org.mozilla.focus.helpers.TestHelper.browserURLbar;
-import static org.mozilla.focus.helpers.TestHelper.createMockResponseFromAsset;
-import static org.mozilla.focus.helpers.TestHelper.pressEnterKey;
-import static org.mozilla.focus.helpers.TestHelper.waitForIdle;
+import static org.mozilla.focus.helpers.EspressoHelper.openMenu;
+import static org.mozilla.focus.helpers.EspressoHelper.openSettings;
 import static org.mozilla.focus.helpers.TestHelper.waitingTime;
-import static org.mozilla.focus.helpers.WebViewFakeLongPress.injectHitTarget;
 
 /**
  * Open new tab and verify that the UI looks like it should.
  */
 @RunWith(AndroidJUnit4.class)
-public class NewTabTest {
+public class OpenNewTabTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule
@@ -82,13 +54,14 @@ public class NewTabTest {
 
             PreferenceManager.getDefaultSharedPreferences(appContext)
                     .edit()
-                    .putBoolean(FIRSTRUN_PREF, true)
+                    .putBoolean(FirstrunFragment.FIRSTRUN_PREF, true)
                     .apply();
             TestHelper.selectGeckoForKlar();
         }
     };
 
-    public String defaultEngine;
+    private String defaultEngine;
+
     @Before
     public void initDefaultBrowser() throws Exception {
         // Open Settings  and select Search submenu
